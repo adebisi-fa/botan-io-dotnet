d32b89f82be6762ae93d2e92988d70f16eab015e
adebisi-fa-hp <adebisi-fa@live.com>
2016-05-12 01:47:35 +0100

    Setup a console application to try out API. Namespace and code refactoring. Inline documentation of implemented API methods.

---
+++ src/api-test/Program.cs
@@ -0,0 +1,36 @@
+﻿using BotanIO.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace api_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var botan = new Botan("botan-io-token-goes-here");
+
+            TrackHundredUniqueUsers(botan);
+            ShortenUrl(botan);
+        }
+
+        private static void ShortenUrl(Botan botan)
+        {
+            botan.ShortenUrl("http://telegram.com", DateTime.UtcNow.Ticks.ToString());
+        }
+
+        private static void TrackHundredUniqueUsers(Botan botan)
+        {
+            for (int i = 0; i < 100; i++)
+                botan.Track(
+                    "FirstTimer",
+                    DateTime.UtcNow.AddDays(-new Random().Next(1, 365)),
+                    DateTime.UtcNow.Ticks.ToString()
+                );
+
+            Console.WriteLine("All 100 users tracked.");
+        }
+    }
+}
--- src/api/Botan.cs
+++ src/api/Botan.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Net.Http;
 
-namespace Botan.IO.Api
+namespace BotanIO.Api
 {
     public class BotanTrackResponse
     {
@@ -22,24 +22,60 @@ namespace Botan.IO.Api
             _client.BaseAddress = new Uri("https://api.botan.io/");
         }
 
-        public string ShortenUrlAsync(string url, string userIds)
+        /// <summary>
+        /// Shortens a url to https://telgr.me/a-unique-token
+        /// </summary>
+        /// <param name="url">The original url.</param>
+        /// <param name="userIdsSeparatedWithComma">The user (or users for group) meant to click the url.  Multiple values must be separated with a comma, ','.</param>
+        /// <returns></returns>
+        public string ShortenUrl(string url, string userIdsSeparatedWithComma)
         {
             return _client.GetStringAsync(
-                $"s/?token={_botanToken}&url={url}&user_ids={userIds}"
+                $"s/?token={_botanToken}&url={url}&user_ids={userIdsSeparatedWithComma}"
             ).Result;
         }
 
-        public BotanTrackResponse Track (string userId, string eventName, object message)
+        /// <summary>
+        /// Tracks an event along with the metadata stored in message.
+        /// </summary>
+        /// <param name="eventName">The event name</param>
+        /// <param name="message">Event metadata.</param>
+        /// <param name="userId">The user who triggers the event.</param>
+        /// <returns></returns>
+        public BotanTrackResponse Track(string eventName, object message, string userId)
         {
             if (message == null)
                 throw new Exception("Message cannot be null.");
 
             var messageString = JsonConvert.SerializeObject(message);
+
             var responseString = _client
-                .PostAsync($"track/?token={_botanToken}&uid={userId}&name={eventName}", new StringContent(messageString))
+                .PostAsync($"track?token={_botanToken}&uid={userId}&name={eventName}", new StringContent(messageString))
                 .Result.Content.ReadAsStringAsync().Result;
 
             return JsonConvert.DeserializeObject<BotanTrackResponse>(responseString);
+        }
+
+        /// <summary>
+        /// Tracks a date cohorts (daily, weekly, monthly, and years) for an event.
+        /// </summary>
+        /// <param name="eventName">The event name.</param>
+        /// <param name="eventDate">The event date.</param>
+        /// <param name="userId">The user who triggers the event.</param>
+        /// <returns></returns>
+        public BotanTrackResponse Track(string eventName, DateTime eventDate, string userId)
+        {
+            return Track(
+                eventName,
+                new
+                {
+                    daily = eventDate.ToString("yyyy-MM-dd"),
+                    weekly = eventDate.AddDays(-(int)eventDate.DayOfWeek).ToString("yyyy-MM-dd"),
+                    monthly = eventDate.ToString("yyyy-MM"),
+                    yearly = eventDate.ToString("yyyy")
+                },
+                userId
+            );
         }
     }
 }
